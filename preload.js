680877d14aa1c96ab8f5a8cf4f0cedf22d5c9307
onlyquix <luis.gaa@scaltel.de>
2026-07-16 11:45:16 +0200

    Expose secure meme fingerprint API

--- preload.js
+++ preload.js
@@ -3,5 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('dashboardAPI', {
   getConfig: () => ipcRenderer.invoke('dashboard:get-config'),
   getNetworkTime: () => ipcRenderer.invoke('dashboard:get-network-time'),
+  fingerprintImage: (url) => ipcRenderer.invoke('dashboard:fingerprint-image', url),
   exitKiosk: () => ipcRenderer.invoke('dashboard:exit-kiosk')
 });

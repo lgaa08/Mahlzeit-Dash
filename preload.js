ff982df322e3a353f0c164659ea8b456683cc1df
onlyquix <luis.gaa@scaltel.de>
2026-07-16 11:38:08 +0200

    Expose resilient network time API

--- preload.js
+++ preload.js
@@ -2,5 +2,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('dashboardAPI', {
   getConfig: () => ipcRenderer.invoke('dashboard:get-config'),
+  getNetworkTime: () => ipcRenderer.invoke('dashboard:get-network-time'),
   exitKiosk: () => ipcRenderer.invoke('dashboard:exit-kiosk')
 });
